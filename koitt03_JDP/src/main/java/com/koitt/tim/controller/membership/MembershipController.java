b5f8d9d2e5b2e06503a44cb94f79447689722aac
ehdtn0321 <ehdtn0321@naver.com>
2020-06-03 16:25:09 +0900

    수정

--- koitt03_JDP/src/main/java/com/koitt/tim/controller/membership/MembershipController.java
+++ koitt03_JDP/src/main/java/com/koitt/tim/controller/membership/MembershipController.java
@@ -1,21 +1,11 @@
 package com.koitt.tim.controller.membership;
 
-<<<<<<< Updated upstream
-=======
 import javax.servlet.http.HttpServletRequest;
 
->>>>>>> Stashed changes
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-<<<<<<< Updated upstream
-@Controller
-public class MembershipController {
-
-	@RequestMapping("membership/login")
-	public String login(Model model) {
-=======
 import com.koitt.tim.dto.member.MemberDto;
 
 @Controller
@@ -23,13 +13,10 @@ public class MembershipController {
 
 	@RequestMapping("login")
 	public String login(MemberDto mdto, Model model) {
->>>>>>> Stashed changes
 
 		return "membership/login";
 	}
 
-<<<<<<< Updated upstream
-=======
 	@RequestMapping("membership/loginForm")
 	public String loginForm(Model model, HttpServletRequest request) {
 
@@ -48,5 +35,4 @@ public class MembershipController {
 		return "membership/join";
 	}
 
->>>>>>> Stashed changes
 }
