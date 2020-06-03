0173a437df910719cf3169777b6e7b9c178e12ff
Mssuk <djqhr490@naver.com>
2020-06-03 18:55:14 +0900

    수정

--- koitt03_JDP/src/main/java/com/koitt/tim/controller/admin/AdminController.java
+++ koitt03_JDP/src/main/java/com/koitt/tim/controller/admin/AdminController.java
@@ -7,13 +7,15 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.koitt.tim.dto.event.EventDto;
 import com.koitt.tim.dto.member.MemberDto;
 import com.koitt.tim.service.admin.AdminService;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("admin")
 public class AdminController {
@@ -21,9 +23,19 @@ public class AdminController {
 	@Autowired
 	private AdminService adminService;
 
+	// memberList 갖고옴
 	@GetMapping("mlist")
 	public List<MemberDto> mlist() {
 		return adminService.getAllMembers();
+	}
+
+	// eventList 갖고옴
+	@GetMapping("elist/{start}/{end}")
+	public List<EventDto> elist(@PathVariable("start") int startPage, @PathVariable("end") int endPage) {
+
+		System.out.println(startPage);
+		System.out.println(endPage);
+		return adminService.getAllEvents(startPage, endPage);
 	}
 
 }
