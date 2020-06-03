6d62ecde58ab537f8d85168992e53273c0eaebbe
Mssuk <djqhr490@naver.com>
2020-06-03 13:55:06 +0900

    페이지나누는방법2

--- koitt03_JDP/src/main/java/com/koitt/tim/dao/event/EventDao.java
+++ koitt03_JDP/src/main/java/com/koitt/tim/dao/event/EventDao.java
@@ -2,6 +2,7 @@ package com.koitt.tim.dao.event;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import com.koitt.tim.dto.event.EventDto;
@@ -10,7 +11,8 @@ import com.koitt.tim.dto.event.EventDto;
 public interface EventDao {
 	List<EventDto> selectFinEvent();
 
-	List<EventDto> selectEvent(int page, int limit, String search, String text);
+	//페이지 나눠서 글 갖고옴
+	List<EventDto> selectEvent(@Param("p1") int start, @Param("p2") int end);
 
 	int getlistCount(String search, String text);
 }
