1b004fbc4702dc9a07083ece2d0b86463b567e3a
ehdtn0321 <ehdtn0321@naver.com>
2020-06-03 19:10:58 +0900

    directory test

--- koitt03_JDP/src/main/java/com/koitt/tim/controller/MainController.java
+++ koitt03_JDP/src/main/java/com/koitt/tim/controller/MainController.java
@@ -2,13 +2,18 @@ package com.koitt.tim.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
 
 	@GetMapping(path = { "/", "main" })
 	public String main() {
-		return "main/main";
+		return "main";
 	}
 
+	@RequestMapping("header")
+	public String footer() {
+		return "common/header";
+	}
 }
--- koitt03_JDP/src/main/webapp/WEB-INF/views/main.jsp
+++ koitt03_JDP/src/main/webapp/WEB-INF/views/main.jsp
@@ -1,6 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-<jsp:include page="../common/header.jsp"/>
+<jsp:include page="common/header.jsp"/>
 <script type="text/javascript">
 $(document).ready(function() {
 	
@@ -366,4 +366,4 @@ $(document).ready(function() {
 	</div>
 	<!-- //mainSection -->
 
-<jsp:include page="../common/footer.jsp"/>
+<jsp:include page="common/footer.jsp"/>
