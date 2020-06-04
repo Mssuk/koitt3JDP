4311d0bf99fa3d35ca8ed2a8b16f5e0a951cb801
Mssuk <djqhr490@naver.com>
2020-06-05 07:12:19 +0900

    파일업로드

--- koitt03_JDP/src/main/java/com/koitt/tim/controller/admin/AdminController.java
+++ koitt03_JDP/src/main/java/com/koitt/tim/controller/admin/AdminController.java
@@ -4,12 +4,18 @@ package com.koitt.tim.controller.admin;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.koitt.tim.dto.coupon.CouponDto;
 import com.koitt.tim.dto.event.EventDto;
@@ -20,6 +26,8 @@ import com.koitt.tim.service.admin.AdminService;
 @RestController
 @RequestMapping("admin")
 public class AdminController {
+
+	Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	@Autowired
 	private AdminService adminService;
@@ -38,8 +46,25 @@ public class AdminController {
 	}
 
 	// couponList 갖고옴
+	@GetMapping("couplist")
 	public List<CouponDto> couplist() {
 		return adminService.getAllCoupons();
 
+	}
+
+	@PostMapping(value = "/elist", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	public ResponseEntity<?> uploadFile(MultipartFile thumbnailImg, MultipartFile contextImg, String startDate,
+			String endDate, String startTime, String endTime, String editorHtml, String coupon) {
+
+		logger.info("{}", thumbnailImg.getOriginalFilename());
+		logger.info("{}", contextImg.getOriginalFilename());
+		logger.info("{}", startDate);
+		logger.info("{}", endDate);
+		logger.info("{}", startTime);
+		logger.info("{}", endTime);
+		logger.info("{}", editorHtml);
+		logger.info("{}", coupon);
+
+		return ResponseEntity.ok().build();
 	}
 }
