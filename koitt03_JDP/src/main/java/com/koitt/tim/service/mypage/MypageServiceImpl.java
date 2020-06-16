2677d4a0dd10df47acd059d7a555a9d15dbda09b
Mssuk <djqhr490@naver.com>
2020-06-16 15:33:19 +0900

    뭐가바뀐거야

--- koitt03_JDP/src/main/java/com/koitt/tim/service/mypage/MypageServiceImpl.java
+++ koitt03_JDP/src/main/java/com/koitt/tim/service/mypage/MypageServiceImpl.java
@@ -1,53 +1,54 @@
 package com.koitt.tim.service.mypage;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.koitt.tim.dao.coupon.CouponDao;
 import com.koitt.tim.dao.member.MemberDao;
 import com.koitt.tim.dao.order.OrderDao;
 import com.koitt.tim.dto.order.OrderListDto;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class MypageServiceImpl implements MypageService {
 
-    @Autowired
-    CouponDao couponDao;
+	@Autowired
+	CouponDao couponDao;
 
-    @Autowired
-    MemberDao memberDao;
+	@Autowired
+	MemberDao memberDao;
 
-    @Autowired
-    OrderDao orderDao;
+	@Autowired
+	OrderDao orderDao;
 
-    @Override
-    public int countCoupon(String id) {
+	@Override
+	public int countCoupon(String id) {
 
-        return couponDao.selectCountCoupon(id);
-    }
+		return couponDao.selectCountCoupon(id);
+	}
 
-    @Override
-    public int havePoint(String id) {
+	@Override
+	public int havePoint(String id) {
 
-        return memberDao.selectMemberPoint(id);
-    }
+		return memberDao.selectMemberPoint(id);
+	}
 
-    @Override
-    public int orderCount(String id) {
+	@Override
+	public int orderCount(String id) {
 
-        return orderDao.selectOrderCount(id);
-    }
+		return orderDao.selectOrderCount(id);
+	}
 
-    @Override
-    public String orderNum(String id) {
-        return orderDao.selectOrderNum(id); // orderNumber 반출
-    }
+	@Override
+	public String orderNum(String id) {
+		return orderDao.selectOrderNum(id); // orderNumber 반출
+	}
 
-    @Override
-    public List<OrderListDto> orderList(String orderNum) {
-        List<OrderListDto> list = orderDao.selectOrderList(orderNum);
-        return list;
+	@Override
+	public List<OrderListDto> orderList(String orderNum) {
+		List<OrderListDto> list = orderDao.selectOrderList(orderNum);
+		return list;
 
-    }
+	}
 }
