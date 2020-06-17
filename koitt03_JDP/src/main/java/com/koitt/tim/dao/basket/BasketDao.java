f78591dab4979c61f960abdc2582e5548299700b
angmu <timtam0399@naver.com>
2020-06-17 13:55:42 +0900

    장바구니 회원용 공사중(공사안해도 돌아가기는 합니다)
    .

--- koitt03_JDP/src/main/java/com/koitt/tim/dao/basket/BasketDao.java
+++ koitt03_JDP/src/main/java/com/koitt/tim/dao/basket/BasketDao.java
@@ -1,10 +1,17 @@
 package com.koitt.tim.dao.basket;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestParam;
+
 import com.koitt.tim.dto.basket.BasketMemberDto;
 
 public interface BasketDao {
 	// 장바구니 1개 불러오기
 	BasketMemberDto selectBasketMember(BasketMemberDto bdto);
+
+	// 장바구니 여럿 불러오기(회원용)
+	List<BasketMemberDto> seleBasketMembers(@RequestParam("id") String id);
 
 	// 장바구니 업데이트
 	void updateBasketMember(BasketMemberDto bdto);
