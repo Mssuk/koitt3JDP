239e541abf74f66367fe41d347042cf0bdf4ed60
junhee4827 <62282058+junhee4827@users.noreply.github.com>
2020-06-23 15:36:56 +0900

    0623 페이징 진행중

--- koitt03_JDP/src/main/java/com/koitt/tim/dao/product/ProductQuestionDao.java
+++ koitt03_JDP/src/main/java/com/koitt/tim/dao/product/ProductQuestionDao.java
@@ -3,6 +3,7 @@ package com.koitt.tim.dao.product;
 import java.util.HashMap;
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
 import com.koitt.tim.dto.product.ProductQuestionDto;
@@ -14,7 +15,7 @@ public interface ProductQuestionDao {
 
 	//List<ProductQuestionDto> selectPQuestion(String pro_num);	// 해당 상품 문의 불러오기 - 이준희
 	int selectQuestionCount(String pro_num);	//해당 상품 문의 카운트
-	List<HashMap<String,Object>> selectQAList(String pro_num);	//해당 상품 문의/질의 불러오기 - 이준희
+	List<HashMap<String,Object>> selectQAList(@Param("p1") String pro_num,@Param("p2") int startNum,@Param("p3") int endNum);	//해당 상품 문의/질의 불러오기 - 이준희
 
 
 }
